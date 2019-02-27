0e82001bbe2a0e5dc12a166b3a7ecc15f9a30a93
oleg <oleg.gavryliuk@ria.com>
2019-02-27 23:47:12 +0200

    Add ui tests (selenide)

--- ui-tests/src/test/java/com/socks/ui/tests/TestLogin.java
+++ ui-tests/src/test/java/com/socks/ui/tests/TestLogin.java
@@ -1,26 +1,38 @@
 package com.socks.ui.tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.exemple.model.UserPayload;
 import com.exemple.services.UserApiService_v2;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
 public class TestLogin {
 
-  private UserApiService_v2 userApiService = new UserApiService_v2();
+  private UserApiService_v2 userApiService_v2 = new UserApiService_v2();
+
+  @BeforeClass
+  public void setUp(){
+      RestAssured.baseURI = "http://142.93.90.9/";
+      Configuration.browser = "chrome";
+  }
 
   @Test
   public void userCanLogin(){
 
-      userApiService.canRegisterUser();
+      userApiService_v2.canRegisterUser();
+      UserPayload userPayload = new UserPayload();
 
       Selenide.open("http://142.93.90.9/");
       Selenide.$("#login > a").click();
-      Selenide.$("#username-modal").sendKeys("");
-      Selenide.$("#password-modal").sendKeys("");
+      Selenide.$("#username-modal").sendKeys(userPayload.getUsername());
+      Selenide.$("#password-modal").sendKeys(userPayload.getPassword());
       Selenide.$("#login-modal p button").click();
 
   }
-  //27:09
 
+//29
+    //33
 }
